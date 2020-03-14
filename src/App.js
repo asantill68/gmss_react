9f1a85f68c13e243bc7b914600646d6d598c4bf9
Alex Santillan <asantill68@yahoo.com>
2020-03-13 22:03:52 -0500

    Clean up front end

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./App.css";
-import Gmss from "./components/Header/";
-import Enter from "./components/Enter/";
-import Intro from "./components/Intro";
-import HouseCard from "./components/HouseCard/";
+import Gmss from "./components/Header";
+import Enter from "./components/Enter";
+import Info from "./components/Info";
+import HouseCard from "./components/HouseCard";
 import homes from "./homes.json";
 
 function App() {
   return (
     <div className="App">
       <Gmss />
-      <Intro />
+      <Info />
       <div>
         <HouseCard
           name={homes[0].name}
---
+++ src/Components/Info/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.css";
+
+const Info = props => {
+  return (
+    <div className="infor">
+      <h1>View our properties.</h1>
+    </div>
+  );
+};
+
+export default Info;
